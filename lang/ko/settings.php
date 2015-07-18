1d0b0ec43663eae95665f3b45b95e44835141ae9
Myeongjin <aranet100@gmail.com>
2015-07-18 19:15:42 +0200

    translation update

---
+++ lang/ko/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['url']                   = 'YouTrack 설치의 기본 URL';
+$lang['user']                  = '로그인 사용자';
+$lang['password']              = '로그인 비밀번호';
+$lang['date_format']           = '날짜 형식';
