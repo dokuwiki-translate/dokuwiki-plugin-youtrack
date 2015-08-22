7c9b82477f218c563b40e7d5b7241b9c6cae4123
Padhie <develop@padhie.de>
2015-08-22 11:15:42 +0200

    translation update

---
+++ lang/de/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Padhie <develop@padhie.de>
+ */
+$lang['user']                  = 'Benutzername';
+$lang['password']              = 'Passwort';
+$lang['date_format']           = 'Datumsformat';
