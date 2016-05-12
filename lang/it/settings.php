9040ff836391ce8d198a79c3a931a0285242633e
OlatusRooc <olatusrooc@virgilio.it>
2016-05-12 10:25:16 +0200

    translation update

---
+++ lang/it/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author OlatusRooc <olatusrooc@virgilio.it>
+ */
+$lang['url']                   = 'Indirizzo URL della tua installazione YouTrack';
+$lang['user']                  = 'Nome di login';
+$lang['password']              = 'Password di login';
+$lang['date_format']           = 'Formato della data';
