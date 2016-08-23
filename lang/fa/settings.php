47282786c711d697257547d5e1e2b2d5742bd37e
Sam01 <m.sajad079@gmail.com>
2016-08-24 00:25:22 +0200

    translation update

---
+++ lang/fa/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Sam01 <m.sajad079@gmail.com>
+ */
+$lang['url']                   = 'پایه URL نصب YouTrack';
+$lang['user']                  = 'شناسه‌کاربری ورود';
+$lang['password']              = 'رمز ورود';
+$lang['date_format']           = 'تاریخ فرمت';
