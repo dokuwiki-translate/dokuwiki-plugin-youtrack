5c07a0646c4fb9ad69b8cfe76eabbe5bb7efe19f
rnck <dokuwiki@rnck.de>
2016-08-23 13:05:06 +0200

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -2,9 +2,11 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Padhie <develop@padhie.de>
+ * @author rnck <dokuwiki@rnck.de>
  */
+$lang['url']                   = 'Basis-URL der YouTrack-Installation';
 $lang['user']                  = 'Benutzername';
 $lang['password']              = 'Passwort';
 $lang['date_format']           = 'Datumsformat';
