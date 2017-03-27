3dce0d6277a4ccda37bf3367d0b1b256d321f327
Farhad <fredfiroozy@gmail.com>
2017-03-27 21:10:05 +0200

    translation update

---
+++ lang/fa/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Farhad <fredfiroozy@gmail.com>
+ */
+$lang['url']                   = 'آدرس URL که یوتراک نصب خواهد شد';
+$lang['user']                  = 'نام کاربری';
+$lang['password']              = 'رمز عبور';
+$lang['date_format']           = 'فرمت تاریخ';
