e11142ab71b484aa54411781233972439b20c2ed
Tor Härnqvist <tor@harnqvist.se>
2017-08-27 12:30:34 +0200

    translation update

---
+++ lang/sv/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Tor Härnqvist <tor@harnqvist.se>
+ */
+$lang['url']                   = 'Bas-URL för YouTrack-installationen';
+$lang['user']                  = 'Användarnamn för inloggning';
+$lang['password']              = 'Lösenord för inloggning';
+$lang['date_format']           = 'Datumformat';
